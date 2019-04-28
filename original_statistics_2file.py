cfbe7ed9e45ec4557dd1b5d1010b2f05a1443e25
Boris Moshailov <Boris.Moshailov@emc.com>
2019-04-28 23:54:43 +0300

    append update

--- original_statistics_2file.py
+++ original_statistics_2file.py
@@ -2,30 +2,11 @@ import key_stitching_functinos as func
 import numpy as np
 
 
-hex2bin_map = {
-       "0":"0000",
-       "1":"0001",
-       "2":"0010",
-       "3":"0011",
-       "4":"0100",
-       "5":"0101",
-       "6":"0110",
-       "7":"0111",
-       "8":"1000",
-       "9":"1001",
-       "a":"1010",
-       "b":"1011",
-       "c":"1100",
-       "d":"1101",
-       "e":"1110",
-       "f":"1111"
-    }
-
 
 if __name__== "__main__":
     #hex_key_2048="023456789abcdef1dcba987654321112233445566778899aabbccddeef1eeddccbbaa99887766554433221100111222333444555666777888999aaabbbcccddd101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e70f808182838485868788898a8b8c8d8e8909192939495969798999a9b9c9d9e91a0a1a2a3a4a5a6a7a8a9aaabacadaea1b0b1b2b3b4b5b6b7b8b9babbbcbdbeb1c0c1c2c3c4c5c6c7c8c9cacbcccdcec1"
     hex_key_500="023456789abcdef1dcba987654321112233445566778899aabbccddeef1eeddccbbaa99887766554433221100111222333444555666777888999aaabbbcccddd"
-    key=''.join(hex2bin_map[i] for i in hex_key_500)
+    key=''.join(func.hex2bin_map[i] for i in hex_key_500)
     #path="C:/YAEL/BGU/data_for_proj/key_stich/good_decoded_samples.txt"
     path="./samples/key500_probe300_good_decoded_samples_486K.txt"
     n=key_length= len(key)
