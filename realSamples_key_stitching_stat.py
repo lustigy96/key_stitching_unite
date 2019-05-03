719e0a9d327278d15aeeb4e76ac8874e5c0f4fa0
Boris Moshailov <Boris.Moshailov@emc.com>
2019-05-03 22:21:55 +0300

    statistics collection real and simulation

--- realSamples_key_stitching_stat.py
+++ realSamples_key_stitching_stat.py
@@ -11,47 +11,55 @@ import os
 
 if __name__ == "__main__":
 
-
+    SIMULATION = False
+    ALLOW_CYCLES = False
     try:
         os.mkdir("./results/")
     except:
         None
 
-    print "\n\n~~Start Gabi Algorithem...~~\n\n"
+    try:
+        os.mkdir("./results/realChannel/")
+    except:
+        None
+
+    print "\n\n~~Start Gabi Algorithem ...~~\n\n"
+
 
 
     hex_key_500="023456789abcdef1dcba987654321112233445566778899aabbccddeef1eeddccbbaa99887766554433221100111222333444555666777888999aaabbbcccddd"
     key=''.join(func.hex2bin_map[i] for i in hex_key_500)
     path = "./samples/key500_probe300_good_decoded_samples_486K.txt"
+    path2 = "./samples/key500_probe300_good_decoded_samples_486KV2.txt"
+    path3 = "./samples/key500_probe300_good_decoded_samples_170Ksamp.txt"
 
 
-
-    window_size_vec=[20,30] #each has its own graph
-    quantile_vec=[0.3,0.5,0.7] #each has its own graph
-    samples_num_vec=[1000]#[1000, 200000, 300000] #bars section
-    start_samp=0
-    result_dict={}
-    ALLOW_CYCLES = True
+    window_size_vec=[30, 20] #each has its own graph
+    quantile_vec=[0.5, 0.7, 0.9] #each has its own graph
+    samples_num_vec=[100000, 300000 , 600000, 800000, 1000000, 2000000] #bars section
     stitch_shift_size = 1
     key_length = n = len(key)
     window_size = 30
-    SIMULATION = False
+    # quantile = 0.5
 
 
-    f_data=open("./results/data.txt","w")
-
+    f_data=open("./results/realChannel/dataForGraph_Key={0}_Graphs=quantile_X=samplesNumVec.txt".format(key_length),"a+")
     f_data.write(' '.join(map(str,quantile_vec)))
     f_data.write('\n')
     f_data.write(' '.join(map(str,samples_num_vec)))
     f_data.write('\n')
+    f_data.close()
+    i = 1
     for quantile in quantile_vec:
+        start_samp = 0
+        result_dict = {}
         for samples_num in samples_num_vec:
-            summryMy = open("./results/summryMy_keyLength={0}_allowCycle={1}_shiftPointersMethod=GabiOptimized_windowSize={2}_simulation={3}.txt".format(key_length, ALLOW_CYCLES, window_size, SIMULATION), "a+")
+            f_data = open("./results/realChannel/dataForGraph_Key={0}_Graphs=quantile_X=samplesNumVec.txt".format(key_length),"a+")
+            summryMy = open("./results/realChannel/summryMy_keyLength={0}_allowCycle={1}_shiftPointersMethod=GabiOptimized_windowSize={2}_simulation={3}.txt".format(key_length, ALLOW_CYCLES, window_size, SIMULATION), "a+")
 
-            print "start real ..."
-            p_list = []
-            p_list.append(path)
+            p_list = [path, path2, path3]
             result_df, result_dict = func.build_samples_from_file(p_list=p_list, window_size=window_size, sample_start=start_samp, sample_end=samples_num, result_dict=result_dict)
+            start_samp = samples_num
             common_samples_df = func.prune_samples_extended(result_df, min_count=-1, quantile=quantile)
             shift_pointers_Boris, all2PowerWindowArray, all2PowerWindowArray_idx, orderArrayMaxToMin = func.build_shift_pointers_position_better(common_samples_df, stitch_shift_size, window_size, ALLOW_CYCLES)
 
@@ -59,32 +67,34 @@ if __name__ == "__main__":
             candidate_key = max(retrieved_key, key=len)
 
             ## print results conclusion to file
-            string = "\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~\nResults ~~~~~~~~ "
+            string = "\nRESULT_NUMBER = {0}".format(i)
             summryMy.write(string)
-            string = "\noriginal key(len={0}):\n".format(n) + key
+            string = "\noriginal key(len={0}):\n".format(key_length) + key
             summryMy.write(string)
             string = "\ncandidate_key(len={0}):\n".format(len(candidate_key)) + candidate_key
             summryMy.write(string)
             string = "\nkey.find(candidate_key) = " + str(key.find(candidate_key))
             summryMy.write(string)
-            string = "\nlevenshtein_edit_dist(key, candidate_key) = \n" + str(func.levenshtein_edit_dist(key, candidate_key))
+            conclusion, s1_match_indices = func.levenshtein_edit_dist(key, candidate_key)
+            string = "\nlevenshtein_edit_dist(key, candidate_key) = \n" + str((conclusion, s1_match_indices))
             summryMy.write(string)
             string = "\nSIMULATION = " + str(SIMULATION)
             summryMy.write(string)
-            string = "\nresult_df = " + str(len(result_df)) + \
+            string = "\nsamples_num = " + str(samples_num) + \
+                     "\nresult_df = " + str(len(result_df)) + \
                      "\ncommon_samples_df quantile = " + str(quantile) + \
                      "\ncommon_samples_df = " + str(len(common_samples_df)) + \
                      "\nstitch_shift_size = " + str(stitch_shift_size) + \
                      "\nwindow_size = " + str(window_size) + \
-                     "\nallowCycle = " + str(ALLOW_CYCLES)
+                     "\nallowCycle = " + str(ALLOW_CYCLES) + \
+                     "\n"
             summryMy.write(string)
             summryMy.close()
 
-
-            dist = func.levenshtein_edit_dist(candidate_key,key, False)[0]
-            f_data.write(str(dist['DIST'])+" "+str(dist['I'])+" "+str(dist['D'])+" "+str(dist['F'])+" "+str(len(key)-len(candidate_key))+"\n")
-
-    f_data.close()
+            dist = conclusion #func.levenshtein_edit_dist(candidate_key,key, False)[0]
+            f_data.write(str(dist['DIST'])+" "+str(dist['I'])+" "+str(dist['D'])+" "+str(dist['F'])+" "+str(len(candidate_key))+"\n")
+            f_data.close()
+            i+=1
 
 
 
---
+++ simulation_key_stitching_stat.py
@@ -0,0 +1,120 @@
+import numpy as np
+import pandas as pd
+import key_stitching_functinos as func
+import os
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    SIMULATION = True
+    ALLOW_CYCLES = False
+    CASE = 1
+    try:
+        os.mkdir("./results/")
+    except:
+        None
+
+    try:
+        os.mkdir("./results/simulation/")
+    except:
+        None
+
+    print "\n\n~~Start Gabi Algorithem...~~\n\n"
+
+
+
+
+
+    key_length_vec = [512,1024,2048]
+    sample_len_vec = [31,40,50]  # [50, 60, 70, 80, 100]
+    megic_num_vec = [50,80,100,150,200]
+    stitch_vec = [1]
+    window_size_vec=[20,30] #each has its own graph
+    quantile_vec=[0.5] #each has its own graph
+
+    stitch_shift_size = 1
+    window_size = 30
+    quantile = 0.5
+    sample_len = 50
+
+
+
+    if CASE==1:
+        flip_probability = 0.05  # (float)(110/500)
+        delete_probability = 0.05  # (float)(20/500)
+        insert_probability = 0.05  # (float)(20/500)
+        f_data = open("./results/simulation/dataForGraph_ErrorProbabilty=5%_Graphs=keyLength_X=megicNumVec.txt", "a+")
+
+
+    if CASE==2:
+        flip_probability = 0.22  # (float)(110/500)
+        delete_probability = 0.05  # (float)(20/500)
+        insert_probability = 0.05  # (float)(20/500)
+        f_data = open("./results/simulation/dataForGraph_ErrorProbabilty=20%_Graphs=keyLength_X=megicNumVec.txt", "a+")
+
+
+    f_data.write(' '.join(map(str,key_length_vec)))
+    f_data.write('\n')
+    f_data.write(' '.join(map(str,megic_num_vec)))
+    f_data.write('\n')
+    f_data.close()
+
+    i=0
+    for key_length in key_length_vec:
+        key = func.init_key(key_length, 41)
+        for megic_num in megic_num_vec:
+            if CASE == 1:
+                f_data = open("./results/simulation/dataForGraph_ErrorProbabilty=5%_Graphs=keyLength_X=megicNumVec.txt","a+")
+            if CASE == 2:
+                f_data = open("./results/simulation/dataForGraph_ErrorProbabilty=20%_Graphs=keyLength_X=megicNumVec.txt","a+")
+            summryMy = open("./results/simulation/summryMy_keyLength={0}_allowCycle={1}_shiftPointersMethod=GabiOptimized_windowSize={2}_simulation={3}.txt".format(key_length, ALLOW_CYCLES, window_size, SIMULATION), "a+")
+
+            samples_num = megic_num * (key_length - sample_len) * (sample_len - window_size)
+            result_df = func.build_samples(key, samples_num, sample_len, window_size, flip_probability, delete_probability, insert_probability, n)
+            common_samples_df = func.prune_samples_extended(result_df, min_count=-1, quantile=quantile)
+            shift_pointers_Boris, all2PowerWindowArray, all2PowerWindowArray_idx, orderArrayMaxToMin = func.build_shift_pointers_position_better(common_samples_df, stitch_shift_size, window_size, ALLOW_CYCLES)
+
+            retrieved_key = func.stitch_boris(common_samples_df, shift_pointers_Boris, all2PowerWindowArray_idx, allowCycle=ALLOW_CYCLES, key_length=key_length)
+            candidate_key = max(retrieved_key, key=len)
+
+            ## print results conclusion to file
+            string = "\nRESULT_NUMBER = {0}".format(i)
+            summryMy.write(string)
+            string = "\noriginal key(len={0}):\n".format(key_length) + key
+            summryMy.write(string)
+            string = "\ncandidate_key(len={0}):\n".format(len(candidate_key)) + candidate_key
+            summryMy.write(string)
+            string = "\nkey.find(candidate_key) = " + str(key.find(candidate_key))
+            summryMy.write(string)
+            conclusion, s1_match_indices = func.levenshtein_edit_dist(key, candidate_key)
+            string = "\nlevenshtein_edit_dist(key, candidate_key) = \n" + str((conclusion, s1_match_indices))
+            summryMy.write(string)
+            string = "\nSIMULATION = " + str(SIMULATION)
+            summryMy.write(string)
+            string = "\nmegic_num = " + str(megic_num) + \
+                     "\nsample_len = " + str(sample_len) + \
+                     "\nsamples_num = megic_num * (key_length - sample_len) * (sample_len - window_size) = " + str(samples_num) + \
+                     "\nresult_df = " + str(len(result_df)) + \
+                     "\ncommon_samples_df quantile = " + str(quantile) + \
+                     "\ncommon_samples_df = " + str(len(common_samples_df)) + \
+                     "\nstitch_shift_size = " + str(stitch_shift_size) + \
+                     "\nwindow_size = " + str(window_size) + \
+                     "\nallowCycle = " + str(ALLOW_CYCLES) + \
+                     "\n"
+            summryMy.write(string)
+            summryMy.close()
+
+            dist = conclusion #func.levenshtein_edit_dist(candidate_key,key, False)[0]
+            f_data.write(str(dist['DIST'])+" "+str(dist['I'])+" "+str(dist['D'])+" "+str(dist['F'])+" "+str(len(candidate_key))+"\n")
+            f_data.close()
+            i+=1
+
+
+
+
+
