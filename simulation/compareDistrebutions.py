95748282d3132bbcf2285bdb3ec2a792f27f9786
ubu <borimos@post.bgu.ac.il>
2019-09-15 16:42:51 +0300

    compute distrebution

--- simulation/compareDistrebutions.py
+++ simulation/compareDistrebutions.py
@@ -9,7 +9,7 @@ from bitstring import BitArray
 
 
 
-def Compute_precitle_of_good_samples(allGoodPossible, result_df, percentileArray, expNum, max, min):
+def Compute_precitle_of_good_samples(allGoodPossible, percentileArray, expNum, max, min, goodDidntShow, rezolution):
     """
     this function computes for specific experiment in which precitles and how many good samples are
     :param allGoodPossible: dict of good sample and its count {"1010100..": count}
@@ -17,36 +17,22 @@ def Compute_precitle_of_good_samples(allGoodPossible, result_df, percentileArray
     :param percentileArray:
     :param expNum:
     """
-
-    precentile10 = (max-min) / 10
+    onePrecentile = (max-min) / rezolution
 
     for goodSample in allGoodPossible.keys():
         if allGoodPossible[goodSample]["count"] == 0:
-            percentileArray["0"][expNum] += 1
-        elif allGoodPossible[goodSample]["count"] > 0 and allGoodPossible[goodSample]["count"] <= 1*precentile10:
-            percentileArray["0-10"][expNum] += 1
-        elif allGoodPossible[goodSample]["count"] > 1*precentile10 and allGoodPossible[goodSample]["count"] <= 2*precentile10:
-            percentileArray["10-20"][expNum] += 1
-        elif allGoodPossible[goodSample]["count"] > 2*precentile10 and allGoodPossible[goodSample]["count"] <= 3*precentile10:
-            percentileArray["20-30"][expNum] += 1
-        elif allGoodPossible[goodSample]["count"] > 3*precentile10 and allGoodPossible[goodSample]["count"] <= 4*precentile10:
-            percentileArray["30-40"][expNum] += 1
-        elif allGoodPossible[goodSample]["count"] > 4*precentile10 and allGoodPossible[goodSample]["count"] <= 5*precentile10:
-            percentileArray["40-50"][expNum] += 1
-        elif allGoodPossible[goodSample]["count"] > 5*precentile10 and allGoodPossible[goodSample]["count"] <= 6*precentile10:
-            percentileArray["50-60"][expNum] += 1
-        elif allGoodPossible[goodSample]["count"] > 6*precentile10 and allGoodPossible[goodSample]["count"] <= 7*precentile10:
-            percentileArray["60-70"][expNum] += 1
-        elif allGoodPossible[goodSample]["count"] > 7*precentile10 and allGoodPossible[goodSample]["count"] <= 8*precentile10:
-            percentileArray["70-80"][expNum] += 1
-        elif allGoodPossible[goodSample]["count"] > 8*precentile10 and allGoodPossible[goodSample]["count"] <= 9*precentile10:
-            percentileArray["80-90"][expNum] += 1
-        elif allGoodPossible[goodSample]["count"] > 9*precentile10:
-            percentileArray["90-100"][expNum] += 1
+            goodDidntShow[expNum] += 1
+        else:
+            for i in range(1, rezolution):
+                if allGoodPossible[goodSample]["count"] > (i-1)*onePrecentile and  allGoodPossible[goodSample]["count"] <= i*onePrecentile:
+                    percentileArray[i-1][expNum] += 1
+            if allGoodPossible[goodSample]["count"] > (rezolution-1)*onePrecentile:
+                percentileArray[rezolution-1][expNum] += 1
+
     return percentileArray
 
 
-def Compute_statistics_for_spesific_error_type(type, numberOfExpiriments, key, fragment_len, fragments_number, window_size, allGoodPossible ):
+def Compute_statistics_for_spesific_error_type(type, numberOfExpiriments, key, fragment_len, fragments_number, window_size, allGoodPossible, rezolution ):
     """this function genertes several expiriments of given error type in order to collect some statistcs
     of the such error: mean value, variance etc ..
 
@@ -63,19 +49,8 @@ def Compute_statistics_for_spesific_error_type(type, numberOfExpiriments, key, f
     varianceArray = np.zeros(numberOfExpiriments, dtype=np.float64)
     maxArray = np.zeros(numberOfExpiriments, dtype=np.int)
     minArray = np.zeros(numberOfExpiriments, dtype=np.int)
-    percentileArray = {}
-    percentile = {}
-    percentileArray["0"] = np.zeros(numberOfExpiriments, dtype=np.uint32)
-    percentileArray["0-10"] = np.zeros(numberOfExpiriments, dtype=np.uint32)
-    percentileArray["10-20"] = np.zeros(numberOfExpiriments, dtype=np.uint32)
-    percentileArray["20-30"] = np.zeros(numberOfExpiriments, dtype=np.uint32)
-    percentileArray["30-40"] = np.zeros(numberOfExpiriments, dtype=np.uint32)
-    percentileArray["40-50"] = np.zeros(numberOfExpiriments, dtype=np.uint32)
-    percentileArray["50-60"] = np.zeros(numberOfExpiriments, dtype=np.uint32)
-    percentileArray["60-70"] = np.zeros(numberOfExpiriments, dtype=np.uint32)
-    percentileArray["70-80"] = np.zeros(numberOfExpiriments, dtype=np.uint32)
-    percentileArray["80-90"] = np.zeros(numberOfExpiriments, dtype=np.uint32)
-    percentileArray["90-100"] = np.zeros(numberOfExpiriments, dtype=np.uint32)
+    goodDidntShow = np.zeros(numberOfExpiriments, dtype=np.int)
+    percentileArray = np.zeros([rezolution, numberOfExpiriments], dtype=np.uint32)
 
     error =  error_dict[type]
     for expNum in xrange(0,numberOfExpiriments):
@@ -87,29 +62,28 @@ def Compute_statistics_for_spesific_error_type(type, numberOfExpiriments, key, f
         varianceArray[expNum] = result_df["count"].var()
         maxArray[expNum] = result_df["count"].max()
         minArray[expNum] = result_df["count"].min()
-        percentileArray = Compute_precitle_of_good_samples(allGoodPossible=allGoodPossible, result_df=result_df,
-                                                           percentileArray=percentileArray, expNum=expNum,
+        percentileArray = Compute_precitle_of_good_samples(allGoodPossible=allGoodPossible,
+                                                           percentileArray=percentileArray,
+                                                           expNum=expNum,
                                                            max=maxArray[expNum],
-                                                           min=minArray[expNum])
-    percentile2 = np.zeros(10, dtype=np.float64)
-    percentile2[0] = percentileArray["0-10"].mean()
-    percentile2[1] = percentileArray["10-20"].mean()
-    percentile2[2] = percentileArray["20-30"].mean()
-    percentile2[3] = percentileArray["30-40"].mean()
-    percentile2[4] = percentileArray["40-50"].mean()
-    percentile2[5] = percentileArray["50-60"].mean()
-    percentile2[6] = percentileArray["60-70"].mean()
-    percentile2[7] = percentileArray["70-80"].mean()
-    percentile2[8] = percentileArray["80-90"].mean()
-    percentile2[9] = percentileArray["90-100"].mean()
-
+                                                           min=minArray[expNum],
+                                                           goodDidntShow=goodDidntShow,
+                                                           rezolution=rezolution)
+    percentile = np.zeros(rezolution, dtype=np.float64)
+    for i in range(0, rezolution):
+        percentile[i] = percentileArray[i].mean()
 
 
     mean = meanArray.mean()
     variance = varianceArray.mean()
     max = maxArray.mean()
     min = minArray.mean()
-    result = {"mean":mean , "variance":variance,"percentile":percentile2, 'goodDidntShowed':percentileArray["0"].mean(), 'max':max, 'min': min}
+    result = {"mean":mean ,
+              "variance":variance,
+              "percentile":percentile,
+              'goodDidntShowed':goodDidntShow.mean(),
+              'max':max,
+              'min': min}
     return result
 
 
@@ -127,48 +101,50 @@ def autolabel(rects):
 if __name__ == "__main__":
 
     error_dict = {
-        "mixed": {"f": 0.05, "d": 0.05, "i": 0.05},
-        "onlyFips": {"f": 0.15, "d": 0.0, "i": 0.0,},
-        "onlyDeletions": {"f": 0.0, "d": 0.15, "i": 0.0},
-        "onlyInsertions": {"f": 0.0, "d": 0.0, "i": 0.15}
+        "mixed": {"f": 0.03, "d": 0.03, "i": 0.03},
+        "onlyFips": {"f": 0.09, "d": 0.0, "i": 0.0},
+        "onlyDeletions": {"f": 0.0, "d": 0.09, "i": 0.0},
+        "onlyInsertions": {"f": 0.0, "d": 0.0, "i": 0.09}
     }
 
     hex_key_2048 = "40554dc4edd210b27e4be5d4d6dcde0f3ab8199730db8a5cf3f3d1617d956cd7dfa0b1e7f82f0b0949d67f7b2b3f84e62537d41eb0142aaf1f84aa6d74b1e0aa2bf82f84298e6d9f6aa580c75905bda8508aad6b73f75862246a7aebe964d543fa05b455b58a3a0f301ab9d9f4232a82e5aaed1303514109f0b4526eb5706c1d3c231e9bd9c96f647774fc923686f17b8707035db6b3f16163154c1d11276540ec776b341fe292def59bcfe161869fae2dc04de17603ae012a3b22d611a3643414e7eff365c8bd3b35323f56759dc6a9dd7704f5d760deb29e8bbd50586b8df7ee9c33d6b6abf9b625635b9db15360c5eae2b89dc4ff443722e5e6b06f71e930"
     key = key2048 = ''.join(func.hex2bin_map[i] for i in hex_key_2048)
     key_length = len(key)
     fragment_len = 40
-    fragments_number = 500000
+    fragments_number = 10000
     window_size = 22
     numberOfExpiriments = 1
-
-    resultDict = {"onlyFips": None, "onlyDeletions": None, "onlyInsertions": None, "mixed": None}
-
-    # fig, ax = plt.subplots()
-    # subp = {
-    #     0: {"r": 0, "c": 0},
-    #     1: {"r": 0, "c": 1},
-    #     2: {"r": 1, "c": 0},
-    #     3: {"r": 1, "c": 1},
-    # }
-
-
+    rezolution = 10
+    resultDict = {
+        "onlyFips": None,
+        "onlyDeletions": None,
+        "onlyInsertions": None,
+        "mixed": None
+    }
 
     allGoodPossible = func.All_possible_window_strings(key=key, window_size=window_size)
 
     for i, type in enumerate(resultDict.keys(), 0):
         result =  Compute_statistics_for_spesific_error_type(type=type, numberOfExpiriments=numberOfExpiriments, key=key,
                                                              fragment_len=fragment_len, fragments_number=fragments_number,
-                                                             window_size=window_size, allGoodPossible=allGoodPossible)
+                                                             window_size=window_size, allGoodPossible=allGoodPossible,
+                                                             rezolution=rezolution)
         resultDict[type] = result
-
         for goodSample in allGoodPossible.keys():
             allGoodPossible[goodSample]["count"] = 0
 
+    #print
+    for type in resultDict.keys():
+        print type
+        print 'max: ' + str(resultDict[type]['max'])
+        print 'min: ' + str(resultDict[type]['min'])
+        print 'goodDidntShowed: ' + str(resultDict[type]['goodDidntShowed'])
 
-    print str(resultDict)
+    labelsF = []
+    pixel = 100/rezolution
+    for i in range(0,rezolution):
+        labelsF.append('({0}-{1})%'.format(i*pixel,(i+1)*pixel))
 
-    labelsF = ['(0-10)%', '(10-20)%', '(20-30)%', '(30-40)%', '(40-50)%', '(50-60)%', '(60-70)%', '(70-80)%',
-               '(80-90)%', '(90-100)%']
     x = np.arange(len(labelsF))  # the label locations
     widthBar = 0.40  # the width of the bars
     fig, ax = plt.subplots()
